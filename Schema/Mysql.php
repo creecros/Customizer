9b3e8ff69022cb228fc9b6cf29fddc53a58964de
Craig Crosby <creecros@gmail.com>
2018-10-16 14:48:16 -0400

    Update Mysql.php

--- Schema/Mysql.php
+++ Schema/Mysql.php
@@ -10,12 +10,14 @@ function version_1(PDO $pdo)
 {
    $pdo->exec("CREATE TABLE customizer_files (
         id INT NOT NULL AUTO_INCREMENT,
+        custom_id INT NOT NULL,
         name VARCHAR(255),
         path VARCHAR(255),
         is_image TINYINT(1) DEFAULT 0,
         date INT NOT NULL DEFAULT 0,
         user_id INT NOT NULL DEFAULT 0,
         size INT NOT NULL DEFAULT 0,
+        FOREIGN KEY(custom_id) REFERENCES customizer(id) ON DELETE CASCADE,
         PRIMARY KEY (id)
     ) ENGINE=InnoDB CHARSET=utf8"
     );
