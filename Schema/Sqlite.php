4aaf97077e17c43fd718ffbb1a1f80dd16018ffe
Craig Crosby <creecros@gmail.com>
2018-10-16 14:46:07 -0400

    Update Sqlite.php

--- Schema/Sqlite.php
+++ Schema/Sqlite.php
@@ -12,12 +12,14 @@ function version_1(PDO $pdo)
 $pdo->exec("
         CREATE TABLE customizer_files (
             id INTEGER PRIMARY KEY,
+            custom_id INTEGER NOT NULL,
             name TEXT COLLATE NOCASE NOT NULL,
             path TEXT NOT NULL,
             is_image INTEGER DEFAULT 0,
             size INTEGER DEFAULT 0 NOT NULL,
             user_id INTEGER DEFAULT 0 NOT NULL,
-            date INTEGER DEFAULT 0 NOT NULL
+            date INTEGER DEFAULT 0 NOT NULL,
+            FOREIGN KEY(custom_id) REFERENCES customizer(id) ON DELETE CASCADE
         )"
     );
     
