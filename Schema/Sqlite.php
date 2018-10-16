7a0717446e5bfe00311456eebb8acb5637818aab
Craig Crosby <creecros@gmail.com>
2018-10-16 14:04:30 -0400

    Create Sqlite.php

---
+++ Schema/Sqlite.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Kanboard\Plugin\Customizer\Schema;
+
+use PDO;
+
+const VERSION = 1;
+
+function version_1(PDO $pdo)
+{
+
+$pdo->exec("
+        CREATE TABLE customizer_files (
+            id INTEGER PRIMARY KEY,
+            name TEXT COLLATE NOCASE NOT NULL,
+            path TEXT NOT NULL,
+            is_image INTEGER DEFAULT 0,
+            size INTEGER DEFAULT 0 NOT NULL,
+            user_id INTEGER DEFAULT 0 NOT NULL,
+            date INTEGER DEFAULT 0 NOT NULL
+        )"
+    );
+    
+}
