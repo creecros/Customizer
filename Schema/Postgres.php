a627f12d6ccd5dcd3160f0530cd07461d7f798f6
Craig Crosby <creecros@gmail.com>
2018-10-16 14:06:13 -0400

    Create Postgres.php

---
+++ Schema/Postgres.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Kanboard\Plugin\Customizer\Schema;
+
+use PDO;
+
+const VERSION = 1;
+
+function version_1(PDO $pdo)
+{
+
+$pdo->exec("
+        CREATE TABLE project_has_files (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(255) NOT NULL,
+            path VARCHAR(255) NOT NULL,
+            is_image BOOLEAN DEFAULT '0',
+            size INTEGER DEFAULT 0 NOT NULL,
+            user_id INTEGER DEFAULT 0 NOT NULL,
+            date INTEGER DEFAULT 0 NOT NULL
+        )"
+    );
+    
+}
