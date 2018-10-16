a2f47049fe510894cd75b0d73ca903c659f895ba
Craig Crosby <creecros@gmail.com>
2018-10-16 14:47:02 -0400

    Update Postgres.php

--- Schema/Postgres.php
+++ Schema/Postgres.php
@@ -12,12 +12,14 @@ function version_1(PDO $pdo)
 $pdo->exec("
         CREATE TABLE customizer_files (
             id SERIAL PRIMARY KEY,
+            custom_id INTEGER NOT NULL,
             name VARCHAR(255) NOT NULL,
             path VARCHAR(255) NOT NULL,
             is_image BOOLEAN DEFAULT '0',
             size INTEGER DEFAULT 0 NOT NULL,
             user_id INTEGER DEFAULT 0 NOT NULL,
-            date INTEGER DEFAULT 0 NOT NULL
+            date INTEGER DEFAULT 0 NOT NULL,
+            FOREIGN KEY(custom_id) REFERENCES customizer(id) ON DELETE CASCADE
         )"
     );
     
