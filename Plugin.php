e363eecac73dedd66dbd94868425f2a5e66742b5
Craig Crosby <creecros@gmail.com>
2018-10-17 11:14:15 -0400

    Create Plugin.php

---
+++ Plugin.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Kanboard\Plugin\Customizer;
+
+use Kanboard\Core\Plugin\Base;
+use Kanboard\Core\Translator;
+use Kanboard\Core\Security\Role;
+
+class Plugin extends Base
+{
+    public function initialize()
+    {
+        $this->template->hook->attach('template:config:sidebar', 'customizer:config/sidebar');
+
+    }
+    
+    public function getClasses() {
+        return array(
+            'Plugin\Customizer\Model' => array(
+                'CustomizerFileModel',
+            )
+        );
+    }
+    
+    public function getPluginName()
+    {
+        return 'Customizer';
+    }
+    
+    public function getPluginDescription()
+    {
+        return t('Add Logo and Flavicon');
+    }
+    
+    public function getPluginAuthor()
+    {
+        return 'Craig Crosby';
+    }
+    
+    public function getPluginVersion()
+    {
+        return '0.0.1';
+    }
+    
+    public function getPluginHomepage()
+    {
+        return 'https://github.com/creecros/Customizer';
+    }
+    
+    public function getCompatibleVersion()
+    {
+        return '>=1.0.42';
+    }
+}
