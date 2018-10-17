4125d6d7fa9c925a60766ed88459f1acbf7ba2cf
Craig Crosby <creecros@gmail.com>
2018-10-17 16:11:47 -0400

    Update title.php

--- Template/header/title.php
+++ Template/header/title.php
@@ -1,7 +1,7 @@
 <h1>
     <?php if (null !== $this->task->customizerFileModel->getByType(1)) : ?>
     <span class="logo">
-      <a href="<?= $this->url->link('K<span>B</span>', 'DashboardController', 'show', array(), false, '', t('Dashboard')) ?>">
+      <a href="<?= $this->url->href('DashboardController', 'show', array(), false, '', t('Dashboard')) ?>">
         <img src="<?= $this->url->href('CustomizerFileController', 'image', array('plugin' => 'customizer', 'file_id' => $this->task->customizerFileModel->getIdByType(1))) ?>" height="50">
       </a>
     </span>
