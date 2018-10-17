a4613f37b0793b954c2c733e31818b794f3e054b
Craig Crosby <creecros@gmail.com>
2018-10-17 11:09:15 -0400

    Create remove.php

---
+++ Template/file/remove.php
@@ -0,0 +1,15 @@
+<div class="page-header">
+    <h2><?= t('Remove a file') ?></h2>
+</div>
+
+<div class="confirm">
+    <p class="alert alert-info">
+        <?= t('Do you really want to remove this file: "%s"?', $this->text->e($file['name'])) ?>
+    </p>
+
+    <?= $this->modal->confirmButtons(
+        'CustomizerFileController',
+        'remove',
+        array('plugin' => 'customizer', 'custom_id' => $file['custom_id'], 'file_id' => $file['id'])
+    ) ?>
+</div>
