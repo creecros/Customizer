37fb6fe7b223e8935b9dc041bc8517dda923590d
Craig Crosby <creecros@gmail.com>
2018-10-17 15:19:23 -0400

    Update sidebar.php

--- Template/config/sidebar.php
+++ Template/config/sidebar.php
@@ -1,3 +1,3 @@
 <li <?= $this->app->checkMenuSelection('ConfigController', 'application') ?>>
-    <?= $this->url->link(t('Application Images'), 'CustomizerFileController', 'show', ['plugin' => 'Customizer']) ?>
+    <?= $this->url->link(t('Customizer'), 'CustomizerFileController', 'show', ['plugin' => 'Customizer']) ?>
 </li>
