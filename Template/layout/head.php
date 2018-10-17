1c71f7e2abf93aa31df450588f6e6c35cc3ab725
Craig Crosby <creecros@gmail.com>
2018-10-17 16:21:01 -0400

    Create head.php

---
+++ Template/layout/head.php
@@ -0,0 +1,5 @@
+<?php if (null !== $this->task->customizerFileModel->getByType(2)) : ?>
+        <link rel="icon" type="image/png" href="<?= $this->url->href('CustomizerFileController', 'image', array('plugin' => 'customizer', 'file_id' => $this->task->customizerFileModel->getIdByType(2))) ?>">
+<?php else: ?>
+        <link rel="icon" type="image/png" href="<?= $this->url->dir() ?>plugins/Themeplus/Img/<?= $themePlusConfig['favicon'] ?>">
+<?php endif; ?>
