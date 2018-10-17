d8f65409e57e27554e278a8a1bcfa6551eec8de1
Craig Crosby <creecros@gmail.com>
2018-10-17 10:33:38 -0400

    Update and rename Template/config/file/show.php to Template/file/show.php

--- Template/config/file/show.php
+++
@@ -1,9 +0,0 @@
-<section class="accordion-section <?= empty($files) && empty($images) ? 'accordion-collapsed' : '' ?>">
-    <div class="accordion-title">
-        <h3><a href="#" class="fa accordion-toggle"></a> <?= t('Assets') ?></h3>
-    </div>
-    <div class="accordion-content">
-        <?= $this->render('customizer:file/logo', array('custom_id' => 1, 'image' => $image)) ?>
-        <?= $this->render('customizer:file/flavicon', array('custom_id' => 2, 'image' => $image)) ?>
-    </div>
-</section>
---
+++ Template/file/show.php
@@ -0,0 +1,27 @@
+<section>
+    <div>
+        <h3><?= t('Assets') ?></h3>
+    </div>
+    <div>
+        <?= $this->render('customizer:file/logo', array('custom_id' => 1, 'image' => $image)) ?>
+    </div>
+     <ul>
+    <?php
+        ini_set('display_errors', 1);
+        ini_set('display_startup_errors', 1);
+        error_reporting(E_ALL);
+    ?>
+    <?= $this->modal->medium('file', t('Upload Logo'), 'CustomizerFileController', 'create', array('plugin' => 'customizer', 'custom_id' => 1))?>
+      </ul>
+    <div>
+        <?= $this->render('customizer:file/flavicon', array('custom_id' => 2, 'image' => $image)) ?>
+    </div>
+     <ul>
+    <?php
+        ini_set('display_errors', 1);
+        ini_set('display_startup_errors', 1);
+        error_reporting(E_ALL);
+    ?>
+    <?= $this->modal->medium('file', t('Upload Flavicon'), 'CustomizerFileController', 'create', array('plugin' => 'customizer', 'custom_id' => 2))?>
+      </ul>
+</section>
