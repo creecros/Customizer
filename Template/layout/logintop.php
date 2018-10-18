bec1430ab982593b6246170301b1140704d671d4
Craig Crosby <creecros@gmail.com>
2018-10-17 23:21:20 -0400

    Create logintop.php

---
+++ Template/layout/logintop.php
@@ -0,0 +1,9 @@
+<?php if (null !== $this->task->customizerFileModel->getByType(1)) : ?>
+
+<div id="login-top">
+	<?php if ($this->configModel->exits('login_link')): ?><a href="<?php echo $this->configModel->getOptions('login_link', 'https://kanboard.org'); ?>" target="_blank"><?php endif ?>
+		<img src="<?= $this->url->href('CustomizerFileController', 'image', array('plugin' => 'customizer', 'file_id' => $this->task->customizerFileModel->getIdByType(1))) ?>" height="50">
+	<?php if ($this->configModel->exits('login_link')): ?></a><?php endif ?>
+</div>
+
+<?php endif ?>
