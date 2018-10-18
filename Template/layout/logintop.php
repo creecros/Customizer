4b4f3d3f395bffc187da72e31f2fa01d05bf81db
Craig Crosby <creecros@gmail.com>
2018-10-18 10:07:25 -0400

    Update logintop.php

--- Template/layout/logintop.php
+++ Template/layout/logintop.php
@@ -1,1 +1,1 @@
- <?= $this->helper->loginHelper->loginpage() ?>
+<?= $this->url->link('<img src="'<?= $this->url->href('CustomizerFileController', 'logo', array('plugin' => 'customizer') ?>"' height="50">', 'CustomizerFileController', 'link', array('plugin' => 'customizer')) ?> 
