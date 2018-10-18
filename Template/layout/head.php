d2f56aea94f69745a7dbab181a830d2febf9f7a6
Craig Crosby <creecros@gmail.com>
2018-10-17 22:04:42 -0400

    Update head.php

--- Template/layout/head.php
+++ Template/layout/head.php
@@ -1,5 +1,5 @@
 <?php if (null !== $this->task->customizerFileModel->getByType(2)) : ?>
-        <link rel="icon" type="image/png" href="<?= $this->url->href('CustomizerFileController', 'image', array('plugin' => 'customizer', 'file_id' => $this->task->customizerFileModel->getIdByType(2))) ?>">
+        <link rel="icon" type="image/png" href="<?= $this->url->href('CustomizerFileController', 'image', array('plugin' => 'customizer', 'file_id' => $this->task->customizerFileModel->getIdByType(2))) ?>" sizes="16x16">
 <?php else: ?>
         <link rel="icon" type="image/png" href="<?= $this->url->dir() ?>plugins/Themeplus/Img/<?= $themePlusConfig['favicon'] ?>">
-<?php endif; ?>
+<?php endif ?>
