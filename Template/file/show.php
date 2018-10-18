1e50dcaf0b0a3a85fe10cdc8b6531b91b031383a
Craig Crosby <creecros@gmail.com>
2018-10-17 23:40:18 -0400

    Update show.php

--- Template/file/show.php
+++ Template/file/show.php
@@ -5,13 +5,17 @@
     <div>
         <img src="<?= $this->url->href('CustomizerFileController', 'image', array('plugin' => 'customizer', 'file_id' => $logo['id'])) ?>" alt="<?= $this->text->e($logo['name']) ?>">
     </div>
+    <br>
      <ul>
     <?php
         ini_set('display_errors', 1);
         ini_set('display_startup_errors', 1);
         error_reporting(E_ALL);
     ?>
-         
+                  
+    <?= $this->modal->medium('file', t('Upload Logo'), 'CustomizerFileController', 'create', array('plugin' => 'customizer', 'custom_id' => 1))?>
+    </ul>
+    <br>     
     <form method="post" action="<?= $this->url->href('ConfigController', 'save', array('redirect' => 'application')) ?>" autocomplete="off">
     <?= $this->form->csrf() ?>
 
@@ -25,12 +29,11 @@
         <button type="submit" class="btn btn-blue"><?= t('Save') ?></button>
     </div>
 </form>     
-         
-    <?= $this->modal->medium('file', t('Upload Logo'), 'CustomizerFileController', 'create', array('plugin' => 'customizer', 'custom_id' => 1))?>
-      </ul>
+<br>
     <div>
         <img src="<?= $this->url->href('CustomizerFileController', 'image', array('plugin' => 'customizer', 'file_id' => $flavicon['id'])) ?>" alt="<?= $this->text->e($flavicon['name']) ?>">
     </div>
+    <br>
      <ul>
     <?php
         ini_set('display_errors', 1);
