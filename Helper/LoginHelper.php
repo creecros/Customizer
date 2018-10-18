98201c7cd25079c00fe232fac1f9d1dc126769ea
Craig Crosby <creecros@gmail.com>
2018-10-18 10:44:16 -0400

    Update LoginHelper.php

--- Helper/LoginHelper.php
+++ Helper/LoginHelper.php
@@ -2,45 +2,19 @@
 
 namespace Kanboard\Plugin\Customizer\Helper;
 
-use Kanboard\Plugin\Customizer\Model\CustomizerFileModel;
-use Kanboard\Plugin\Customizer\Controller\CustomizerFileController;
-use Kanboard\Model\ConfigModel;
 use Kanboard\Core\Base;
 
 class LoginHelper extends Base
 {
-
-    public function logo()
-    {
-        $file = $this->customizerFileModel->getByType(1);
-        $this->customizerFileController->renderFileWithCache($file, $this->helper->file->getImageMimeType($file['name']));
-    }
     
-    public function link()
+    public function loginlogo($logo, $link)
     {
-        return $this->configModel->getOptions('login_link', 'https://kanboard.org');
-    }
-    
-    public function logoexists()
-    {
-        if (null !== $this->customizerFileModel->getByType(1)) { return true; } else { return false; }  
-    }
-   
-    public function linkexists()
-    {
-        if ($this->configModel->exists('login_link')) { return true; } else { return false; }  
-    }
-    
-    public function loginpage()
-    {
-        if ($this->logoexists() && $this->linkexists()) {
-            return 	'<a href="" target="_blank"><?php endif ?>
-		             <img src="' . $this->logo() . '" height="50">
-	                 </a>';
-        } else if ($this->logoexists()) {
-            return '<img src="' . $this->logo() . '" height="50">';
-        } else {
-            return '';         
+	if (isset($link)){
+        	return '<a href="'.$this->helper->text->e($link).'><img src="'.$logo.'" height="50"></a>';
+	} else if (isset($logo)) {
+        	return '<img src="'.$logo.'" height="50">';
+	} else {
+		return '';
 	}
     }
     
