2085a4c6c1278942cc93dccc37170f6381d8e41b
Craig Crosby <creecros@gmail.com>
2018-10-18 11:55:47 -0400

    Update upload_flavicon.php

--- Template/file/upload_flavicon.php
+++ Template/file/upload_flavicon.php
@@ -3,12 +3,12 @@
 </div>
 
 <?= $this->app->component('file-upload', array(
-    'maxSize'           => $max_size,
+    'maxSize'           => 10000,
     'url'               => $this->url->to('CustomizerFileController', 'save', array('plugin' => 'customizer', 'custom_id' => 2)),
     'labelDropzone'     => t('Drag and drop your file here'),
     'labelOr'           => t('or'),
     'labelChooseFiles'  => t('choose file'),
-    'labelOversize'     => t('The maximum allowed file size is %sB.', $this->text->bytes($max_size)),
+    'labelOversize'     => t('The maximum allowed file size is %sB.', $this->text->bytes(10000)),
     'labelSuccess'      => t('File has been uploaded successfully.'),
     'labelCloseSuccess' => t('Close this window'),
     'labelUploadError'  => t('Unable to upload this file.'),
