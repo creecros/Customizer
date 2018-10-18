1d9cc0a3bf359add52d01660f54c38f9c64cf1e2
Craig Crosby <creecros@gmail.com>
2018-10-18 02:08:55 -0400

    Update logintop.php

--- Template/layout/logintop.php
+++ Template/layout/logintop.php
@@ -1,7 +1,2 @@
-<?php if (null !== $this->task->customizerFileModel->getByType(1)) : ?>
-
-	<?php if ($this->task->configModel->exists('login_link')): ?><a href="<?php echo $this->configModel->getOptions('login_link', 'https://kanboard.org'); ?>" target="_blank"><?php endif ?>
-		<img src="<?= $this->url->href('CustomizerFileController', 'image', array('plugin' => 'customizer', 'file_id' => $this->task->customizerFileModel->getIdByType(1))) ?>" height="50">
-	<?php if ($this->task->configModel->exists('login_link')): ?></a><?php endif ?>
-
-<?php endif ?>
+<?= $this->url->href('CustomizerFileController', 'loginpage', array('plugin' => 'customizer')) ?>
+	
