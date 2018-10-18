507cde0f97d3662b3dfebb20d3aef8b2fb281872
Craig Crosby <creecros@gmail.com>
2018-10-18 11:55:27 -0400

    Update upload_logo.php

--- Template/file/upload_logo.php
+++ Template/file/upload_logo.php
@@ -3,12 +3,12 @@
 </div>
 
 <?= $this->app->component('file-upload', array(
-    'maxSize'           => $max_size,
+    'maxSize'           => 20000,
     'url'               => $this->url->to('CustomizerFileController', 'save', array('plugin' => 'customizer', 'custom_id' => 1)),
     'labelDropzone'     => t('Drag and drop your file here'),
     'labelOr'           => t('or'),
     'labelChooseFiles'  => t('choose file'),
-    'labelOversize'     => t('The maximum allowed file size is %sB.', $this->text->bytes($max_size)),
+    'labelOversize'     => t('The maximum allowed file size is %sB.', $this->text->bytes(20000)),
     'labelSuccess'      => t('File has been uploaded successfully.'),
     'labelCloseSuccess' => t('Close this window'),
     'labelUploadError'  => t('Unable to upload this file.'),
