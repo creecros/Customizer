2f9d740e13e05762463aeab65126a39da713e4fe
Craig Crosby <creecros@gmail.com>
2018-10-20 15:22:44 -0400

    Create upload_loginlogo.php

---
+++ Template/file/upload_loginlogo.php
@@ -0,0 +1,20 @@
+<div class="page-header">
+    <h2><?= t('Login Logo') ?></h2>
+</div>
+
+<?= $this->app->component('file-upload', array(
+    'maxSize'           => 500000,
+    'url'               => $this->url->to('CustomizerFileController', 'save', array('plugin' => 'customizer', 'custom_id' => 1)),
+    'labelDropzone'     => t('Drag and drop your file here'),
+    'labelOr'           => t('or'),
+    'labelChooseFiles'  => t('choose file'),
+    'labelOversize'     => t('The maximum allowed file size is %sB.', $this->text->bytes(500000)),
+    'labelSuccess'      => t('File has been uploaded successfully.'),
+    'labelCloseSuccess' => t('Close this window'),
+    'labelUploadError'  => t('Unable to upload this file.'),
+)) ?>
+
+<?= $this->modal->submitButtons(array(
+   'submitLabel' => t('Upload Login Logo'),
+   'disabled'    => true,
+)) ?>
