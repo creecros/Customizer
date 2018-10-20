9704c8d4856ac0799294abda42b8d6d41f514f2f
Craig Crosby <creecros@gmail.com>
2018-10-20 15:58:49 -0400

    Update CustomizerFileController.php

--- Controller/CustomizerFileController.php
+++ Controller/CustomizerFileController.php
@@ -67,6 +67,7 @@ class CustomizerFileController extends BaseController
     {
         $logo = $this->customizerFileModel->getByType(1);
         $flavicon = $this->customizerFileModel->getByType(2);
+	$loginlogo = $this->customizerFileModel->getByType(3);
         $logopath = $logo['path'];
         $flaviconpath = $flavicon['path'];
         $this->response->html($this->helper->layout->config('customizer:file/show', array(
@@ -75,6 +76,7 @@ class CustomizerFileController extends BaseController
             'flavicon' => $flavicon,
             'logopath' => $logopath,
             'flaviconpath' => $flaviconpath, 
+            'loginlogo' => $loginlogo
         )));
         
     }
