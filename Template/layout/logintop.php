c9755872c6890d8f307b211bc894e71f7c69661b
Craig Crosby <creecros@gmail.com>
2018-10-20 22:54:23 -0400

    Update logintop.php

--- Template/layout/logintop.php
+++ Template/layout/logintop.php
@@ -3,7 +3,6 @@
 <?= $this->url->link('<img src="' . $this->url->href('CustomizerFileController', 'loginlogo', array('plugin' => 'customizer')) .  '" height="50">', 'CustomizerFileController', 'link', array('plugin' => 'customizer')) ?> 
 <?php endif ?>
 <style>
-  <style>
 body  {
     background-image: url("https://source.unsplash.com/random");
     background-size:     cover;                   
