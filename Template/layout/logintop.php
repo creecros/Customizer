119dc2812911bf40fef3ed8398c152746832d462
Craig Crosby <creecros@gmail.com>
2018-10-20 22:58:26 -0400

    Update logintop.php

--- Template/layout/logintop.php
+++ Template/layout/logintop.php
@@ -1,10 +1,11 @@
 <?php global $loginCheck; ?>
+<?php global $backURL; ?>
 <?php if ($loginCheck): ?>
 <?= $this->url->link('<img src="' . $this->url->href('CustomizerFileController', 'loginlogo', array('plugin' => 'customizer')) .  '" height="50">', 'CustomizerFileController', 'link', array('plugin' => 'customizer')) ?> 
 <?php endif ?>
 <style>
 body  {
-    background-image: url("https://source.unsplash.com/random");
+    background-image: url("<?= $backURL ?>");
     background-size:     cover;                   
     background-repeat:   no-repeat;
 }
