640deff2cc9fb7d26a1c4c39ecfe5ac1c8e21a98
Craig Crosby <creecros@gmail.com>
2018-10-20 23:10:28 -0400

    Update Plugin.php

--- Plugin.php
+++ Plugin.php
@@ -55,7 +55,7 @@ class Plugin extends Base
     
     public function getPluginVersion()
     {
-        return '0.0.1';
+        return '0.0.2';
     }
     
     public function getPluginHomepage()
