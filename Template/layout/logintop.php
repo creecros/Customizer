52cfa10df07ae3e55ee38cfd7d69fc56f16e3915
Craig Crosby <creecros@gmail.com>
2018-10-20 22:19:48 -0400

    Update logintop.php

--- Template/layout/logintop.php
+++ Template/layout/logintop.php
@@ -1,7 +1,4 @@
 <?php global $loginCheck; ?>
 <?php if ($loginCheck): ?>
-<style>
-body {background-image: <?= $this->url->href('CustomizerFileController', 'loginlogo', array('plugin' => 'customizer')) ?> }
-</style>
 <?= $this->url->link('<img src="' . $this->url->href('CustomizerFileController', 'loginlogo', array('plugin' => 'customizer')) .  '" height="50">', 'CustomizerFileController', 'link', array('plugin' => 'customizer')) ?> 
 <?php endif ?>
