4942a441d5bdce1ff09adaaaf11ef21d0cfee656
Craig Crosby <creecros@gmail.com>
2018-10-21 00:19:46 -0400

    Update logintop.php

--- Template/layout/logintop.php
+++ Template/layout/logintop.php
@@ -4,9 +4,8 @@
 <?= $this->url->link('<img src="' . $this->url->href('CustomizerFileController', 'loginlogo', array('plugin' => 'customizer')) .  '" height="50">', 'CustomizerFileController', 'link', array('plugin' => 'customizer')) ?> 
 <?php endif ?>
 <style>
-body  {
-    background-image: url("<?= $backURL ?>");
-    background-size:     cover;                   
-    background-repeat:   no-repeat;
+html  {
+    background: url("<?= $backURL ?>") no-repeat center center fixed;
+    background-size:     cover;
 }
 </style>
