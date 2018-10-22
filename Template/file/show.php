7ff4c1d2c42a8a32b7263b05c8a8b2f064d86850
aljawaid <jawaid_imran@hotmail.com>
2018-10-22 20:32:15 +0100

    NEW: Styling Changes
    - Style login form on login page
    - Fixes #3
    - Styled plugin settings page for better user experience
    - Add more css classes for specific styling
    - FIX: Title in page header to support consistency
    - NEW: Titles per section in plugin settings
    - NEW: Colour-coded file upload/remove icons for better user experience

--- Template/file/show.php
+++ Template/file/show.php
@@ -1,13 +1,16 @@
 <?php global $backURL; ?>
 <section>
     <div class="page-header">
-        <h3><?= t('Assets') ?></h3>
+        <h2><?= t('Assets') ?></h2>
     </div>
     <div class="panel header-logo-panel">
+    	<div class="panel-heading">
+    		<h3 class="panel-title"><?= t('Header') ?></h3>
+    	</div>
         <img src="<?= $this->url->href('CustomizerFileController', 'logo', array('plugin' => 'customizer', 'file_id' => $logo['id'])) ?>" alt="<?= $this->text->e($logo['name']) ?>" height="100">
     <br>
     <br>
-     <ul>
+     <ul class="upload-link">
     <?php
         ini_set('display_errors', 1);
         ini_set('display_startup_errors', 1);
@@ -15,9 +18,9 @@
     ?>
                   
     <?= $this->modal->medium('file', t('Upload Header Logo'), 'CustomizerFileController', 'create', array('plugin' => 'customizer', 'custom_id' => 1))?>
-    </ul>
+    </ul class="remove-link">
     <?php if (null !== $this->task->customizerFileModel->getByType(1)) : ?>
-    <ul>
+    <ul class="remove-link">
     <?php
         ini_set('display_errors', 1);
         ini_set('display_startup_errors', 1);
@@ -30,10 +33,13 @@
     </div>    
     <br> 
         <div class="panel login-logo-panel">
+        	<div class="panel-heading">
+    		<h3 class="panel-title"><?= t('Login') ?></h3>
+    	</div>
         <img src="<?= $this->url->href('CustomizerFileController', 'loginlogo', array('plugin' => 'customizer', 'file_id' => $loginlogo['id'])) ?>" alt="<?= $this->text->e($loginlogo['name']) ?>" height="75">
     <br>
     <br>
-     <ul>
+     <ul class="upload-link">
     <?php
         ini_set('display_errors', 1);
         ini_set('display_startup_errors', 1);
@@ -43,7 +49,7 @@
     <?= $this->modal->medium('file', t('Upload Login Logo'), 'CustomizerFileController', 'create', array('plugin' => 'customizer', 'custom_id' => 3))?>
     </ul>
     <?php if (null !== $this->task->customizerFileModel->getByType(3)) : ?>
-    <ul>
+    <ul class="remove-link">
     <?php
         ini_set('display_errors', 1);
         ini_set('display_startup_errors', 1);
@@ -58,13 +64,16 @@
     <form class="url-links" method="post" action="<?= $this->url->href('ConfigController', 'save', array('redirect' => 'application')) ?>" autocomplete="off">
     <?= $this->form->csrf() ?>
     <?php $backURL = $this->task->configModel->get('background_url', '') ?>
-    <fieldset>
+    <fieldset class="login-link-block">
+    	<div class="panel-heading">
+    		<h3 class="panel-title links-title"><?= t('Links') ?></h3>
+    	</div>
         <?= $this->form->label(t('Login Link'), 'login_link') ?>
         <?= $this->form->text('login_link', $values, $errors, array('placeholder="https://example.kanboard.org/"')) ?>
-        <p class="form-help login-link-desc"><?= t('Example: https://example.kanboard.org/ (used as logo link on login page)') ?></p>
+        <p class="form-help login-link-desc"><?= e('Example: <code>https://example.kanboard.org/</code> (used as logo link on login page)') ?></p>
         <?= $this->form->label(t('Login Background Image URL'), 'background_url') ?>
         <?= $this->form->text('background_url', $values, $errors, array('placeholder="https://source.unsplash.com/random"')) ?>
-        <p class="form-help background-img-link-desc"><?= t('Example: https://source.unsplash.com/random (URL for a background image on the login page, centered, autoscale, no-repeat)') ?></p>
+        <p class="form-help background-img-link-desc"><?= e('Example: <code>https://source.unsplash.com/random</code> (URL for a background image on the login page, centered, autoscale, no-repeat)') ?></p>
     </fieldset>
 
     <div class="form-actions">
@@ -73,10 +82,13 @@
 </form>     
 <br>
     <div class="panel favicon-panel">
+    	<div class="panel-heading">
+    		<h3 class="panel-title"><?= t('Favicon') ?></h3>
+    	</div>
         <img src="<?= $this->url->href('CustomizerFileController', 'icon', array('plugin' => 'customizer', 'file_id' => $flavicon['id'])) ?>" alt="<?= $this->text->e($flavicon['name']) ?>" height="50">
     <br>
     <br>
-     <ul>
+     <ul class="upload-link">
     <?php
         ini_set('display_errors', 1);
         ini_set('display_startup_errors', 1);
@@ -86,7 +98,7 @@
       </ul>
     </ul>
     <?php if (null !== $this->task->customizerFileModel->getByType(2)) : ?>
-    <ul>
+    <ul class="remove-link">
     <?php
         ini_set('display_errors', 1);
         ini_set('display_startup_errors', 1);
