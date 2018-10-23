c9d5bc733c87499e529563fdfa1dcc13eb0cadaf
Craig Crosby <creecros@gmail.com>
2018-10-23 09:55:05 -0400

    Create CustomizerConfigController.php

---
+++ Controller/CustomizerConfigController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Kanboard\Plugin\Customizer\Controller;
+
+use Kanboard\Model\ConfigModel;
+use Kanboard\Model\LanguageModel;
+use Kanboard\Plugin\Controller\CustomizerFileController;
+
+/**
+ * Config Controller
+ *
+ * @package  Customizer/Controller
+ * @author   creecros
+ */
+class CustomizerConfigController extends BaseController
+{
+    
+    /**
+     * Save settings
+     *
+     */
+    public function save()
+    {
+        $values =  $this->request->getValues();
+        $redirect = $this->request->getStringParam('redirect', 'application');
+
+        switch ($redirect) {
+            case 'application':
+                $values += array('password_reset' => 0);
+                break;
+            case 'project':
+                $values += array(
+                    'subtask_restriction' => 0,
+                    'subtask_time_tracking' => 0,
+                    'cfd_include_closed_tasks' => 0,
+                    'disable_private_project' => 0,
+                );
+                break;
+        }
+
+        if ($this->configModel->save($values)) {
+            $this->languageModel->loadCurrentLanguage();
+            $this->flash->success(t('Settings saved successfully.'));
+        } else {
+            $this->flash->failure(t('Unable to save your settings.'));
+        }
+
+        $this->response->redirect($this->helper->url->to('ConfigController', $redirect));
+    }
+
+    /**
+     * Display the Customizer settings page
+     *
+     * @access public
+     */
+    public function application()
+    {
+        $this->customizerFileController->show();
+    }
+
+    
+}
