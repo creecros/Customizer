22d0a09e3517b6c1074068baacdfe2c7c1efc4a1
Craig Crosby <creecros@gmail.com>
2018-10-24 16:52:14 -0400

    Update CustomizerFileController.php
    Fixes #16

--- Controller/CustomizerFileController.php
+++ Controller/CustomizerFileController.php
@@ -159,19 +159,16 @@ class CustomizerFileController extends BaseController
         if ($custom_id == 1) {
             $this->response->html($this->template->render('customizer:file/upload_logo', array(
             'custom_id' => $custom_id,
-            'max_size' => $this->helper->text->phpToBytes(get_upload_max_size()),
 	    'multiple' => false,
         )));
         } else if ($custom_id == 2) {
             $this->response->html($this->template->render('customizer:file/upload_flavicon', array(
             'custom_id' => $custom_id,
-            'max_size' => $this->helper->text->phpToBytes(get_upload_max_size()),
 	    'multiple' => false,
         )));
         } else if ($custom_id == 3) {
             $this->response->html($this->template->render('customizer:file/upload_loginlogo', array(
             'custom_id' => $custom_id,
-            'max_size' => $this->helper->text->phpToBytes(get_upload_max_size()),
 	    'multiple' => false,
         )));
 	}
