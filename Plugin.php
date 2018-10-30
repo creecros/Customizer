6666f3c6aae1c7c86fe31dd5330dcf99acc9cf0b
Craig Crosby <creecros@gmail.com>
2018-10-30 13:36:18 -0400

    Update Plugin.php

--- Plugin.php
+++ Plugin.php
@@ -15,39 +15,52 @@ class Plugin extends Base
 	    
 	// Themes
 	$customizer['themes'] = array(
-		'Default' => 'https://raw.githubusercontent.com/creecros/Customizer/Theme-Presets/Assets/css/theme.css',
+		'Default' => '',
 		'Nebula' => 'https://raw.githubusercontent.com/kenlog/Nebula/master/Assets/css/nebula.css',
 		'Moon' => 'https://raw.githubusercontent.com/kenlog/Moon/master/Assets/css/moon.css',
 		'Oxygen' => 'https://raw.githubusercontent.com/kenlog/Oxygen/master/Assets/css/oxygen.css',
 		'KanboardCSS' => 'https://raw.githubusercontent.com/aljawaid/KanboardCSS/master/kanboardcss.css'
 		);
 	    
+	if ($this->configModel->get('themeSelection', '') == '') {
+        file_put_contents('/var/www/app/plugins/Customizer/Assets/css/theme.css', '', 'r'));
+	} else {
         file_put_contents('/var/www/app/plugins/Customizer/Assets/css/theme.css', fopen($this->configModel->get('themeSelection', ''), 'r'));
-
+	}
+	    
         $this->hook->on('template:layout:css', array('template' => 'plugins/Customizer/Assets/css/theme.css'));
 	    
         //Helper
         $this->helper->register('themeHelper', '\Kanboard\Plugin\Customizer\Helper\ThemeHelper');
 	    
+	//Check if login logo is set
         if (null !== $this->customizerFileModel->getByType(3)) { 
 		    $customizer['loginCheck'] = true;
 	    } else { 
 		    $customizer['loginCheck'] = false;
 	    } 
 	    
+	//Grabs login page settings from database   
 	$customizer['backURL'] = $this->configModel->get('background_url', '');
 	$customizer['backColor'] = $this->configModel->get('loginbackground_color', '#ffffff');
 	$customizer['logoSize'] = $this->configModel->get('loginlogo_size', '50');
 	$customizer['loginpanel_color'] = $this->configModel->get('loginpanel_color', '#ffffff');
-        
+	    
+	    
+        //Templates and Assets
         $this->template->hook->attach('template:config:sidebar', 'customizer:config/sidebar');
         $this->template->setTemplateOverride('header/title', 'customizer:header/title');
         $this->template->setTemplateOverride('layout', 'customizer:layout/layout');
         $this->template->setTemplateOverride('auth/index', 'customizer:layout/index');
         $this->template->hook->attach('template:auth:login-form:before', 'customizer:layout/logintop');
-        $this->route->addRoute('settings/customizer', 'CustomizerFileController', 'show', 'Customizer');
-        $this->applicationAccessMap->add('CustomizerFileController', array('loginlogo', 'logo', 'link', 'logoexists', 'linkexists'), Role::APP_PUBLIC);
         $this->hook->on('template:layout:css', array('template' => 'plugins/Customizer/Template/customizer.css'));
+	    
+        //Routes
+        $this->route->addRoute('settings/customizer', 'CustomizerFileController', 'show', 'Customizer');
+	    
+	//Permissions for login page to access logos    
+        $this->applicationAccessMap->add('CustomizerFileController', array('loginlogo', 'logo', 'link', 'logoexists', 'linkexists'), Role::APP_PUBLIC);
+	    
     }
     
     public function getClasses() {
