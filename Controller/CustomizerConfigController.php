dcdb2629c88bc620596065d1f424c0bc0d7dcbae
vistree <fischerk@web.de>
2018-10-30 22:08:06 +0100

    removed unneeded lines from save function

--- Controller/CustomizerConfigController.php
+++ Controller/CustomizerConfigController.php
@@ -22,26 +22,7 @@ class CustomizerConfigController extends BaseController
     public function save()
     {
         $values =  $this->request->getValues();
- /*
-  * is this really needed?
-  */
- /*
-        $redirect = $this->request->getStringParam('redirect', 'application');
 
-        switch ($redirect) {
-            case 'application':
-                $values += array('password_reset' => 0);
-                break;
-            case 'project':
-                $values += array(
-                    'subtask_restriction' => 0,
-                    'subtask_time_tracking' => 0,
-                    'cfd_include_closed_tasks' => 0,
-                    'disable_private_project' => 0,
-                );
-                break;
-        }
-*/
         if ($this->configModel->save($values)) {
             $this->languageModel->loadCurrentLanguage();
             $this->flash->success(t('Settings saved successfully.'));
@@ -51,7 +32,4 @@ class CustomizerConfigController extends BaseController
 
           $this->response->redirect($this->helper->url->to('CustomizerFileController', 'show', array('plugin' => 'Customizer')));
     }
-
-
-    
 }
