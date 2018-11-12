27e7fa83d88c4f600922ce5b3452ed5c100adacc
Craig Crosby <creecros@gmail.com>
2018-11-12 15:35:13 -0500

    Update customizer.js

--- Assets/js/customizer.js
+++ Assets/js/customizer.js
@@ -21,7 +21,7 @@ for (i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function() {
         /* Toggle between adding and removing the "active" class,
         to highlight the button that controls the panel */
-        this.classList.toggle("active");
+        //this.classList.toggle("active");
 
         /* Toggle between hiding and showing the active panel */
         var panel = this.nextElementSibling;
