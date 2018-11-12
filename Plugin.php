7e1978d057037163e8d2eab1a269ac1cfd700d60
Craig Crosby <creecros@gmail.com>
2018-11-12 14:39:26 -0500

    Update Plugin.php

--- Plugin.php
+++ Plugin.php
@@ -67,7 +67,7 @@ class Plugin extends Base
         $this->hook->on('template:layout:css', array('template' => 'plugins/Customizer/Assets/rgbaColorPicker/rgbaColorPicker.css'));
         $this->hook->on('template:layout:js', array('template' => 'plugins/Customizer/Assets/rgbaColorPicker/rgbaColorPicker.js'));
         $this->hook->on('template:layout:css', array('template' => 'plugins/Customizer/Template/customizer.css'));
-
+        $this->hook->on('template:layout:js', array('template' => 'plugins/Customizer/Assets/js/customizer.js'));
 	    
 	//Routes
         $this->route->addRoute('settings/customizer', 'CustomizerFileController', 'show', 'Customizer');
