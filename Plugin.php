01b086e88adaf425099b8bd232a9ceb443e046e3
Craig Crosby <creecros@gmail.com>
2018-11-13 22:20:34 -0500

    Update Plugin.php

--- Plugin.php
+++ Plugin.php
@@ -70,7 +70,7 @@ class Plugin extends Base
         $this->hook->on('template:layout:css', array('template' => 'plugins/Customizer/Assets/css/customizer.css'));
         $this->hook->on('template:layout:js', array('template' => 'plugins/Customizer/Assets/js/customizer.js'));
 	 
-	if ($this->configModel->get('use_custom_login', '')) {
+	if ($this->configModel->get('use_custom_login', '') == 'checked') {
         	$this->template->hook->attach('template:auth:login-form:before', 'customizer:layout/logintop');
 	}
 
