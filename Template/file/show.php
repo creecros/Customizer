dada5e54c1264ebc52e9338831e4e92035d05ed8
Craig Crosby <creecros@gmail.com>
2018-11-15 12:14:26 -0500

    Update show.php

--- Template/file/show.php
+++ Template/file/show.php
@@ -135,7 +135,7 @@ global $customizer;
                 <th width="25%"><strong><?= t('Use Custom Login Settings') ?></strong></th>
                 <th>
                     <label class="switch">
-                    <input onclick="this.form.submit();" name="use_custom_login" type="checkbox" value="checked" <?= $this->task->configModel->get('use_custom_login','') ?>>
+                    <input onchange="this.form.submit();" name="use_custom_login" type="checkbox" value="checked" <?= $this->task->configModel->get('use_custom_login','') ?>>
                     <span class="slider round"></span>
                     </label>
                 </th>
