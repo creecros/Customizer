c7ca15a488a2bf6d0320183e66149528b063b7c7
Craig Crosby <creecros@gmail.com>
2018-11-15 15:20:25 -0500

    Update themecreator.php

--- Template/config/themecreator.php
+++ Template/config/themecreator.php
@@ -1,7 +1,9 @@
 <form name="themecreator" id="themecreator" class="url-links" method="post" action="<?= $this->url->href('CustomizerConfigController', 'create_theme', array('plugin' => 'customizer', 'redirect' => 'application')) ?>" autocomplete="off">
 <?= $this->form->csrf() ?>
   <div class="column-100">
-    <?= t('Theme Name: ') ?><input type="text" name="theme_name" placeholder="mytheme.css" pattern="[a-z0-9._%+-]+\.[a-z]{3}$"><br>
+    <?= t('Theme Name: ') ?><input type="text" name="theme_name" placeholder="mytheme.css" pattern="[a-z0-9._%+-]+\.[a-z]{3}$">
+    <br>
+    <br>
     <table>
       <tr>
         <th colspan="2">
