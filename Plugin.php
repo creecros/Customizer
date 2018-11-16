c686ba579ea5b3fd1be1bd7e9b6232659afd685b
Craig Crosby <creecros@gmail.com>
2018-11-15 21:47:32 -0500

    Update Plugin.php

--- Plugin.php
+++ Plugin.php
@@ -20,10 +20,12 @@ class Plugin extends Base
 	
 	$scanned_preset_themes = array_diff(scandir('plugins/Customizer/Assets/css/themes'), array('..', '.'));
 	$scanned_user_themes = array_diff(scandir(DATA_DIR . '/files/customizer/themes'), array('..', '.'));
-	$scanned_themes = array_merge($scanned_preset_themes, $scanned_user_themes);
 	    
-	foreach ($scanned_themes as $theme) {
+	foreach ($scanned_preset_themes as $theme) {
 		$customizer['themes'][rtrim($theme, '.css')] = 'plugins/Customizer/Assets/css/themes/' . $theme;
+	}
+	foreach ($scanned_user_themes as $theme) {
+		$customizer['themes'][rtrim($theme, '.css')] = DATA_DIR . '/files/customizer/themes/' . $theme;
 	}
 	    
 	if ($this->configModel->get('themeSelection', '') == '') {
