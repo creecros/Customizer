43c557f874685fce084a310692de24fe9065089d
Craig Crosby <creecros@gmail.com>
2018-11-27 09:14:21 -0500

    Update show.php

--- Template/file/show.php
+++ Template/file/show.php
@@ -42,7 +42,7 @@ global $customizer;
         <table>
             <tr>
                 <th width="25%"><strong><?= t('Header Logo Size') ?></strong></th>
-                <th><input type="range" name="headerlogo_size" min="20" max="100" value="<?= $this->task->configModel->get('headerlogo_size','30') ?>">
+                <th><input type="range" name="headerlogo_size" min="20" max="500" value="<?= $this->task->configModel->get('headerlogo_size','30') ?>">
                     <header_logo_output> <?= $this->task->configModel->get('headerlogo_size','30') ?></header_logo_output><?= t('&nbsp;pixels high') ?>
                 </th>
             </tr>
