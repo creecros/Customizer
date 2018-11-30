8f885b3f30c9cef0b3670d7b62f957df11fcd5fc
Craig Crosby <creecros@gmail.com>
2018-11-30 11:54:17 -0500

    Update CustomizerConfigController.php

--- Controller/CustomizerConfigController.php
+++ Controller/CustomizerConfigController.php
@@ -233,10 +233,14 @@ class CustomizerConfigController extends BaseController
             ]
         ); 
         $css->add_raw('
+        .project-header {margin-bottom: 8px;margin-top: 8px;}
         .panel{background-color: #efefef8c;border: 1px solid #efefef89;}
         .task-board{border-width: 3px;background: none!important;}
         div.task-board-recent {border-width: 3px;}
-        table td {border: 1px solid #efefef32;}
+        table td {border:none;}
+        table th {border:none;}
+        table th:first-child {border-top-left-radius:8px;}
+        table th:last-child {border-top-right-radius:8px;}
         .table-list-header{background:#efefef8c;border:1px solid #efefef42;border-radius:5px 5px 0 0;line-height:28px;padding-left:3px;padding-right:3px;}
         .table-list-row{padding-left:3px;padding-right:3px;border-bottom:1px solid #efefef42;border-right:1px solid #efefef42;}
         .table-list-row.table-border-left{border-left:1px solid #efefef42;}
