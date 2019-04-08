acef062f161706f4847b1105b647dd9954d2232c
Craig Crosby <creecros@gmail.com>
2019-04-08 18:57:06 -0400

    Update DynamicAvatar.php

--- Helper/DynamicAvatar.php
+++ Helper/DynamicAvatar.php
@@ -20,7 +20,7 @@ class DynamicAvatar extends AvatarHelper
         } else {
             $html = $this->avatarManager->render($user_id, $username, $name, $email, $avatar_path, $size);
         }
-        return '<div class="avatar avatar-dyn '.$css.'">'.$html.'</div>';
+        return '<div id="'.$css.'" class="avatar avatar-dyn '.$css.'">'.$html.'</div>';
     }
 
     public function dynamic($user_id, $username, $name, $email, $avatar_path, $css = '', $size)
@@ -34,25 +34,5 @@ class DynamicAvatar extends AvatarHelper
         return $this->dynamic($user['id'], $user['username'], $user['name'], $user['email'], $user['avatar_path'], $css, $this->configModel->get('av_radius', '50'));
     }
     
-    public function dynamicRenderPreview($user_id, $username, $name, $email, $avatar_path, $css = 'avatar-left', $size = 48)
-    {
-        if (empty($user_id) && empty($username)) {
-            $html = $this->avatarManager->renderDefault($size);
-        } else {
-            $html = $this->avatarManager->render($user_id, $username, $name, $email, $avatar_path, $size);
-        }
-        return '<div id="al1" class="avatar avatar-dyn '.$css.'">'.$html.'</div>';
-    }
-
-    public function dynamicPreview($user_id, $username, $name, $email, $avatar_path, $css = '', $size)
-    {
-        return $this->dynamicRenderPreview($user_id, $username, $name, $email, $avatar_path, $css, $size);
-    }
-
-    public function currentUserDynamicPreview($css = '')
-    {
-        $user = $this->userSession->getAll();
-        return $this->dynamicPreview($user['id'], $user['username'], $user['name'], $user['email'], $user['avatar_path'], $css, $this->configModel->get('av_radius', '50'));
-    }
     
  }
