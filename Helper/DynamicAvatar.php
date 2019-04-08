2384997e9e998207bbaf6e47c5bb0d1482b42fd9
Craig Crosby <creecros@gmail.com>
2019-04-08 16:13:53 -0400

    Create DynamicAvatar.php

---
+++ Helper/DynamicAvatar.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Kanboard\Plugin\Customizer\Helper;
+
+use Kanboard\Helper\AvatarHelper;
+use Kanboard\Core\Base;
+/**
+ * Avatar Helper
+ *
+ * @package helper
+ * @author  Craig Crosby
+ */
+class DynamicAvatar extends AvatarHelper
+{
+
+    public function dynamic($user_id, $username, $name, $email, $avatar_path, $css = '', $size)
+    {
+        return $this->render($user_id, $username, $name, $email, $avatar_path, $css, $size);
+    }
+
+    public function currentUserSmall($css = '')
+    {
+        $user = $this->userSession->getAll();
+        return $this->dynamic($user['id'], $user['username'], $user['name'], $user['email'], $user['avatar_path'], $css, $this->configModel->get('av_radius', '50'));
+    }
+    
+ }
