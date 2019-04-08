4d6a35a275e9a6463cd56677d09519b01728bb9a
Craig Crosby <creecros@gmail.com>
2019-04-08 19:21:01 -0400

    Update show.php

--- Template/file/show.php
+++ Template/file/show.php
@@ -137,7 +137,7 @@ global $customizer;
             <tr>
                 <th width="25%"><strong><?= t('Avatar Icon Radius') ?></strong></th>
                 <th><input type="range" name="av_radius" min="25" max="50" value="<?= $this->task->configModel->get('av_radius','50') ?>">
-                    <av_radius_output> <?= $this->task->configModel->get('av_radius','50') ?></av_radius_output><?= t('&nbsp;pixels') ?>
+                    <av_radius_output> <?= $this->task->configModel->get('av_radius','50') ?></av_radius_output><?= t('&nbsp;%') ?>
                 </th>
             </tr>
         </table>
