87b4c02a73376bd8d8f3d1ff99e461a5cc79f522
Craig Crosby <creecros@gmail.com>
2019-05-16 10:13:07 -0400

    Update Plugin.php

--- Plugin.php
+++ Plugin.php
@@ -118,16 +118,16 @@ class Plugin extends Base
         Translator::load($this->languageModel->getCurrentLanguage(), __DIR__.'/Locale');
         $user_id = $this->customizerFileModel->getUserSessionId();
         
-        if ($user_id == 0 && $this->configModel->get('customizer_refresh_page', 'no')) { 
+        if ($user_id != 0 && $this->configModel->get('customizer_refresh_page', 'no') == 'logged') { $this->configModel->save(['customizer_refresh_page' => 'no']); }
+    
+        if ($user_id == 0 && $this->configModel->get('customizer_refresh_page', 'no') == 'no') { 
             $this->configModel->save(['customizer_refresh_page' => 'yes']);
             $this->response->redirect($this->helper->url->to('DashboardController', 'show'));
-        } else if ($user_id == 0 && $this->configModel->get('customizer_refresh_page', 'yes')) {
-            $this->configModel->save(['customizer_refresh_page' => 'no']);
+        } else if ($user_id == 0 && $this->configModel->get('customizer_refresh_page', 'no') == 'yes') {
+            $this->configModel->save(['customizer_refresh_page' => 'logged']);
             $this->response->redirect($this->helper->url->to('AuthController', 'login'));
-        } else {
-            $this->configModel->save(['customizer_refresh_page' => 'no']);
-        }
-        
+        } 
+       
         $user_theme = $this->userMetadataModel->get($user_id, 'themeSelection', $this->configModel->get('themeSelection', 'plugins/Customizer/Assets/css/theme.css' ));
         $default_theme = $this->configModel->get('themeSelection', 'plugins/Customizer/Assets/css/theme.css');
         if ($this->configModel->get('toggle_user_themes', 'disable') == 'enable') {
