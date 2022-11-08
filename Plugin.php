b299fa4ec05cef7e8dfda70c76f95b80041e7d58
root <creecros@gmail.com>
2022-11-08 15:43:43 +0000

    Fix #139

--- Plugin.php
+++ Plugin.php
@@ -161,7 +161,7 @@ class Plugin extends Base
     
     public function getPluginVersion()
     {
-        return '1.14.0';
+        return '1.14.1';
     }
     
     public function getPluginHomepage()
--- Template/header/title_older_kb.php
+++ Template/header/title_older_kb.php
@@ -1,7 +1,7 @@
 <h1>
     <?php if (null !== $this->task->customizerFileModel->getByType(1)) : ?>
     <span class="logo">
-      <a href="<?= $this->url->href('DashboardController', 'show', array(), false, '', t('Dashboard')) ?>">
+      <a href="<?= $this->url->href('DashboardController', 'show', array(), false, '', t('Dashboard')) ?>" title="Dashboard">
         <img src="<?= $this->url->href('CustomizerFileController', 'image', array('plugin' => 'customizer', 'file_id' => $this->task->customizerFileModel->getIdByType(1))) ?>" height="<?= $this->task->configModel->get('headerlogo_size', '30') ?>">
       </a>
     </span>
